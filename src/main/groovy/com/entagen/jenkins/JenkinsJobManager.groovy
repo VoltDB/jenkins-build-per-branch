4808c7572a6fe651dbe5d5a74da7cbea16b81291
Phil Rosegay <prosegay@voltdb.com>
2017-12-19 16:12:00 -0500

    set backport for release-7.9

--- src/main/groovy/com/entagen/jenkins/JenkinsJobManager.groovy
+++ src/main/groovy/com/entagen/jenkins/JenkinsJobManager.groovy
@@ -18,7 +18,7 @@ class JenkinsJobManager {
     JenkinsApi jenkinsApi
     GitApi gitApi
 
-    String ALL_RELEASES = "release-\\d+\\.\\d+\\.x(?<!7\\.8\\.x)"
+    String ALL_RELEASES = "release-\\d+\\.\\d+\\.x(?<!7\\.9\\.x)"
     String ALL_BACKPORTS = "-backportv\\d+\\.\\d+"
     String BACKPORT = "-backportv"
 
