95062d27e942d7063f39071d8faaeab5c51ae151
janette <jfournier@voltdb.com>
2024-06-24 11:14:03 -0400

    Update version to 13.3.x

--- src/main/groovy/com/entagen/jenkins/JenkinsJobManager.groovy
+++ src/main/groovy/com/entagen/jenkins/JenkinsJobManager.groovy
@@ -20,7 +20,7 @@ class JenkinsJobManager {
     JenkinsApi2 jenkinsApi
     GitApi gitApi
 
-    String ALL_RELEASES = "release-\\d+\\.\\d+\\.x(?<!13\\.2\\.x)"
+    String ALL_RELEASES = "release-\\d+\\.\\d+\\.x(?<!13\\.3\\.x)"
     //String ALL_RELEASES = "release-\\d+\\.\\d+\\.x"
     String ALL_BACKPORTS = "-backportv\\d+\\.\\d+"
     String BACKPORT = "-backportv"
